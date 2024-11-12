bb367931f052454d32fa432e9a58409d75e7affc
Kevin Ankerstein <kankerstein1@nait.ca>
2024-11-12 12:51:51 -0700

    added comments

--- ICA06/SERVER/SERVER/ClientSideFiles/js/ica06.js
+++ ICA06/SERVER/SERVER/ClientSideFiles/js/ica06.js
@@ -1,42 +1,62 @@
 let menuList= {};
 $(()=>{
     //onload
+
+    //url for welcome message
     let url ="http://localhost:5013/get";
     let welcome = "welcome";
+
+    //data object
     let data = {
         welcome: welcome
     }
+
+    //ajax calls for onload to populate welcome message and the order location, menu and prices
     Ajax(url,"GET",data,"HTML",SuccessWelcome,errorFunc);
     Ajax("http://localhost:5013/locations","POST",data,"JSON",SuccessLocation,errorFunc);
     Ajax("http://localhost:5013/menu","POST",data,"JSON",SuccessMenu,errorFunc);
     Ajax("http://localhost:5013/prices","POST",data,"JSON",SuccessPrice,errorFunc);
+
+    //event handler for clicking submit button
     $("#submit").on("click",function()
     {
+        //returns false in backend?????
         let name = $("#name").val();
+
+        //store order details to populate data object
         let location = $("#locate").val();
         let item = $("#items").val();
         let quantity = $("#amount").val();
         let payment = $("#payment").val();
-        console.log(name);
+        console.log(item);
+
+        //checking for valid inputs for form handling
         if(name = "" || location == "" || item == "" || quantity == "" || payment == "")
         {
             alert("Please fill out all fields");
             return;
         }
+
+        //checking for valid quantity
         if(quantity <= 0)
         {
             alert("Please enter a valid quantity");
             return;
         }
 
+        //order url
         let url = "http://localhost:5013/order";
+
+        //order data object
         let data = {
-            postName: name,
-            postLocation: location,
-            postItem: item,
-            postQuantity: quantity,
-            postPayment: payment
+            "postName": `${$("#name").val()}`,
+            "postLocation": location,
+            "postItem": item,
+            "postQuantity": quantity,
+            "postPayment": payment
         }
+
+        //make ajax call
         Ajax(url,"POST",data,"JSON",SuccessOrder,errorFunc);
     })
     
@@ -49,47 +69,86 @@ function errorFunc(ajaxReq, ajaxStatus, errorThrown)
     console.log(errorThrown);
 
 }
+
+//function to processs welcome ajax call
 function SuccessWelcome(returnData, status)
 {
     $("#welcome").html(returnData);
 }
+
+//function to process succesfull location ajax call
 function SuccessLocation(returnData, status)
 {
     console.log(returnData);
+    //populating select tag with data returned from server
     let locations = "<br><select id='locate'>";
     for (let i=0; i<returnData.length; i++)
     {
         locations += "<option value='"+returnData[i] + "'>"+returnData[i] + "</option>";
     }
     locations += "</select>";
+
+    //append location to the div
     $("#location").append(locations);
 }
+
+//function to handle successive menu data returned from server
 function SuccessMenu(returnData, status)
 {
+    //start of ordered list 
     let menu = "<ol>"
+
+    //populate list with returned items
     for(let item in returnData)
     {
         menuList[item] = returnData[item];
         menu += "<li>"+returnData[item]+"</li>";
     }
     menu += "</ol>";
+
+    //append menu to the menu div
     $("#menu").append(menu);
 }
+
+//function for populating the prices
 function SuccessPrice(returnData, status)
 {
     console.log(menuList);
     let prices = "";
+    //populate options with the menu items
     for(let i = 0; i < returnData.length; i++)
     {
-        prices += "<option value='"+returnData[i] + "'>"+menuList[i] +"</option>";
+        prices += "<option value='"+menuList[i] + "'>"+menuList[i] +"</option>";
     }
     //onsole.log(prices);
+    //append items to the form
     $("#items").append(prices);
 }
+
+//function to post the order sent to the server and update the user with the details
 function SuccessOrder(returnData, status)
 {
-    console.log(returnData);
+    //building order done string
+    let orderDone = "";
+
+    //populating header with name to thank user
+    orderDone +=`<h2>Thank You ${returnData.name} For The Order</h2><br>`;
+
+    //building ordered list with details of the order
+    orderDone +="<ol>";
+    orderDone +=`<li>${returnData.location}</li>`;
+    orderDone +=`<li>${returnData.item}</li>`;
+    orderDone +=`<li>${returnData.quantity}</li>`;
+    orderDone +=`<li>${returnData.payment}</li>`;
+    orderDone += "</ol><br>";
+
+    //adding the time it will take order to be picked up
+    orderDone += `<p>Your order will be ready for pickup in ${returnData.time} minutes`;
+    //append details to the page
+    $("#orderDone").html(orderDone);
 }
+
+//ajax function to send data to the server
 function Ajax(url, method, data, dataType, success, errMehtod)
 {
     let ajOp={};
--- ICA06/SERVER/SERVER/Program.cs
+++ ICA06/SERVER/SERVER/Program.cs
@@ -1,26 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
 namespace SERVER
 {
     public class Program
     {
-        record Info(string postName, string postLocation, string postItem, int postQuantity, string postPayment);
+        public static Random rnd = new Random();    //new random object
+
+        //record to store the data from the post request
+        record Info(string postName, string postLocation, string postItem, string postQuantity, string postPayment);
         public static void Main(string[] args)
         {
-            
+            //create a new builder
             var builder = WebApplication.CreateBuilder(args);
 
+            //add controllers
             builder.Services.AddControllers();
 
+            //build the app
             var app = builder.Build();
 
-            ;
+            //allow cors
             app.UseCors(x=>x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin=> true));
+
+            //map the get request to the root
             app.MapGet("/", () => "ICA 06");
-                
+
+            //map the get request to the /get path and return a welcome message
             app.MapGet("/get", () =>
             {
                 return "<h2>Welcome To Tim Hortons<h2>";
             });
 
+            //map the post request to the /locations path and return an array of locations
             app.MapPost("/locations", () =>
             {
                 string[] locations = new string[] { "Edmonton", "Calgary", "Red Deer"};
@@ -28,6 +38,7 @@ namespace SERVER
                 return locations;
             });
 
+            //map the post request to the /menu path and return an array of menu items
             app.MapPost("/menu", () =>
             {
                 string[] menu = new string[] { "Muffins: $2.29", "Croissant: $2.19", "Cookies: $1.49", "Donut: $1.99", "Iced Cap: $2.49", "Coffee: $2.09"};
@@ -36,25 +47,35 @@ namespace SERVER
                 return menu;
             });
 
+            //map the post request to the /prices path and return an array of prices
             app.MapPost("/prices", () =>
             {
                 double[] prices = new double[] {2.29,2.19,1.49,1.99,2.49,2.09};
                 return prices;
             });
 
+            //map the post request to the /order path and return the order details
             app.MapPost("/order", (Info data) =>
             {
+                //generate a random time taken to prepare the order
+                int timeTaken = rnd.Next(5, 31);
                 Console.WriteLine(data.postName);
+
+                //create a response object
                 var response = (object)new
                 {
                     name = data.postName,
                     location = data.postLocation,
                     item = data.postItem,
                     quantity = data.postQuantity,
-                    payment = data.postPayment
+                    payment = data.postPayment,
+                    time = timeTaken
+
                 };
                 return response;
             });
+
+            //run the app
             app.Run();
         }
     }
